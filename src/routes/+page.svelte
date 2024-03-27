88cc631c247b1d00831b0af17eb0b74047141a4b
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-03-26 21:22:25 -0300

    Commit Inicial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+  <title>O Jogo Da Forca</title>
+  <meta name="description" content="O jogo da forca oferece uma experiência envolvente e educativa, onde os usuários podem desfrutar de desafios de palavras enquanto aprimoram suas habilidades linguísticas. Com uma interface intuitiva e recursos bem projetados, proporciona uma experiência de jogo fluida e imersiva." />
+  <meta name="keywords" content="Jogo, Hangman Game, Jogo Forca, Forca Game"/>
+</svelte:head>
