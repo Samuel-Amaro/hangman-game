cdf812d6a7300e17a67434dbfb8a293ca85d4a64
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-04-04 22:08:49 -0300

    feat: adiciona componente `KeyboardLetter`

---
+++ src/lib/KeyboardLetter.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	export let letter: string;
+	export let disabled = false;
+</script>
+
+<button
+	type="button"
+	title={`Letra ${letter}`}
+	aria-label={`Letra ${letter}`}
+	on:click|preventDefault
+	on:keydown
+	class:disabled
+	{disabled}>{letter}</button
+>
+
+<style>
+	button {
+		appearance: none;
+		font-family: inherit;
+		box-sizing: border-box;
+		border: none;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 8px;
+		width: 28px;
+		height: 56px;
+		background-color: white;
+		color: var(--dark-navy);
+		font-size: 24px;
+		line-height: 24px;
+		letter-spacing: -0.32px;
+		cursor: pointer;
+	}
+
+	.disabled,
+	button:disabled {
+		opacity: 0.25;
+		pointer-events: none;
+	}
+
+	button:is(:hover, :focus) {
+		background-color: var(--blue);
+		color: white;
+	}
+</style>
