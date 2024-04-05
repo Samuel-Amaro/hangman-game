75a2091a0c6bc987865ccc70b585918b5b38a3da
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-04-04 22:07:45 -0300

    add `utils`

---
+++ src/utils/index.ts
@@ -0,0 +1,41 @@
+export const categorys = [
+	{ label: 'FILMES', route: 'filmes' },
+	{ label: 'PROGRAMAS DE TELEVISÃO', route: 'programas-de-televisao' },
+	{ label: 'PAÍSES', route: 'paises' },
+	{ label: 'CIDADES CAPITAIS', route: 'cidades-capitais' },
+	{ label: 'ANIMAIS', route: 'animais' },
+	{ label: 'ESPORTES', route: 'esportes' }
+];
+
+export function randomIdx(max: number) {
+	return Math.floor(Math.random() * max);
+}
+
+export const alphabet =  [
+	"A", "Á", "À", "Â", "Ã", 
+	"B",
+	"C", "Ç",
+	"D",
+	"E", "É", "È", "Ê",
+	"F",
+	"G",
+	"H",
+	"I", "Í", "Ì", "Î",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O", "Ó", "Ò", "Ô", "Õ",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U", "Ú", "Ù", "Û",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z"
+];
