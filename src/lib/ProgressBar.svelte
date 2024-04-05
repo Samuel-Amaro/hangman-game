ed211ff1d180e4d182c64f2af6e751d5deb769bc
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-04-04 22:09:55 -0300

    feat: adiciona componente `ProgressBar`

---
+++ src/lib/ProgressBar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let min: number;
+	export let max: number;
+	export let value: number;
+
+	function calculatePercentFill(min: number, max: number, value: number) {
+		return (100 * (value - min)) / (max - min);
+	}
+</script>
+
+<div
+	role="progressbar"
+	aria-label="Barra de vida"
+	aria-valuenow={value}
+	aria-valuemin={min}
+	aria-valuemax={max}
+	title="Barra de vida"
+>
+	<span style="width: {calculatePercentFill(min, max, value)}%;"></span>
+</div>
+
+<style>
+	div {
+		height: 16px;
+		min-width: 57px;
+		border-radius: 96px;
+		padding: 4px;
+		background-color: white;
+		overflow: hidden;
+	}
+
+	span {
+		display: block;
+		border-radius: 96px;
+		background-color: var(--dark-navy);
+		height: 8px;
+		transition: width ease-in-out 0.3s;
+	}
+</style>
