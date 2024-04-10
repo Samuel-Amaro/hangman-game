fa0a368a2d074ce1f7e438b8b1bbea497a9fb248
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-04-09 22:21:50 -0300

    adiciona `utils`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
+import { browser } from "$app/environment";
+import type { JsonValue } from "$types";
+import { writable } from "svelte/store";
+
 export const categorys = [
 	{ label: 'FILMES', route: 'filmes' },
 	{ label: 'PROGRAMAS DE TELEVISÃO', route: 'programas-de-televisao' },
@@ -11,31 +15,75 @@ export function randomIdx(max: number) {
 	return Math.floor(Math.random() * max);
 }
 
-export const alphabet =  [
-	"A", "Á", "À", "Â", "Ã", 
-	"B",
-	"C", "Ç",
-	"D",
-	"E", "É", "È", "Ê",
-	"F",
-	"G",
-	"H",
-	"I", "Í", "Ì", "Î",
-	"J",
-	"K",
-	"L",
-	"M",
-	"N",
-	"O", "Ó", "Ò", "Ô", "Õ",
-	"P",
-	"Q",
-	"R",
-	"S",
-	"T",
-	"U", "Ú", "Ù", "Û",
-	"V",
-	"W",
-	"X",
-	"Y",
-	"Z"
+export const alphabet = [
+	'A',
+	'Á',
+	'À',
+	'Â',
+	'Ã',
+	'B',
+	'C',
+	'Ç',
+	'D',
+	'E',
+	'É',
+	'È',
+	'Ê',
+	'F',
+	'G',
+	'H',
+	'I',
+	'Í',
+	'Ì',
+	'Î',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'Ó',
+	'Ò',
+	'Ô',
+	'Õ',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'Ú',
+	'Ù',
+	'Û',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z'
 ];
+
+export function localStore<T extends JsonValue>(key: string, initial: T) {
+
+  const toString = (value: T) => JSON.stringify(value, null, 2); 
+  const toObj = JSON.parse; 
+	let saved: any = initial
+
+	if(browser) {
+		if (localStorage.getItem(key) === null) {
+			localStorage.setItem(key, toString(initial));  
+		}
+
+		saved = toObj(localStorage.getItem(key) as string); 
+	}
+
+  const { subscribe, set, update } = writable<T>(saved); 
+
+  return {
+    subscribe,
+    set: (value: T) => {
+      localStorage.setItem(key, toString(value)); 
+      return set(value);
+    },
+    update,
+  };
+};
