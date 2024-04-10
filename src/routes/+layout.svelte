d92857f7d1e653b1495a6849af1c6bf68fa37d97
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-04-09 22:36:30 -0300

    feat: adiciona página `+layout.svelte`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	import '../styles/index.css';
+	import Alert from '$lib/Alert.svelte';
+	import { onMount } from 'svelte';
+	import { visitCounter } from '$lib/stores/data';
+
+	onMount(() => {
+		$visitCounter = $visitCounter + 1
+	})
 </script>
 
 <slot />
+<Alert />
