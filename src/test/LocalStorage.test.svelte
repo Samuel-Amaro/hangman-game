970327964aade332bcfa7c4044ba6345b204c99d
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-04-21 15:58:59 -0300

    test: adiciona `test unit` para `functions utils`

---
+++ src/test/LocalStorage.test.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { localStore } from "$lib/utils";
+
+  const test = localStore('test', 0);
+</script>
+
+<p>$test</p>
---
+++ src/test/utils.test.ts
@@ -0,0 +1,29 @@
+import { afterEach, describe, expect, test, vi } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import { cleanup, render, screen } from '@testing-library/svelte';
+import { calculatePercentFill, getTemplateOgImage } from '$lib/utils';
+import LocalStorage from './LocalStorage.test.svelte';
+
+describe('utils', () => {
+  afterEach(() => {
+    localStorage.clear();
+    cleanup()
+  });
+  
+  test('testa function getTemplateOgImage', () => {
+    const template = getTemplateOgImage('Teste Título', '/test')
+    
+    expect(template).toMatch(/Teste Título/)
+    expect(template).toMatch(/\/test/)
+  })
+
+  test('testa function calculatePercentFill', () => {
+    expect(calculatePercentFill(0, 2, 1)).toBe(50)
+  })
+  
+  test('testa function localStore', () => {
+    render(LocalStorage)
+
+    expect(screen.getByText('0')).toBe('0')
+  })
+})
